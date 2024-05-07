00f7e4c1ffc0be6586070e013b1c7ba5d732c13a
Tineni Baeyens <btineni@gmail.com>
2024-05-07 16:44:48 +0200

    bouton de la page d'accueil pour accéder à la page profil

--- lib/main.dart
+++ lib/main.dart
@@ -70,73 +70,59 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-                style : TextStyle(
-                  fontFamily : 'VT323'
-              )
+            Align(
+              alignment: Alignment.topCenter,
+              child: SizedBox(
+                width: 60.0,
+                height: 60.0,
+                child: RawMaterialButton(
+                  onPressed: _incrementCounter,
+                  elevation: 2.0,
+                  fillColor: Colors.blue,
+                  shape: const CircleBorder(),
+                  child: Image.asset(
+                    "assets/images/development.png",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
             ),
-
-            Image.asset("assets/images/development.png"),
-
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset("assets/images/development.png"),
+                  const Text(
+                    'You have pushed the button this many times:',
+                    style: TextStyle(
+                      fontFamily: 'VT323',
+                    ),
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headlineMedium,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: RawMaterialButton (
-        onPressed: _incrementCounter,
-        elevation : 2.0,
-        fillColor : Colors.blue,
-        shape : const CircleBorder(),
-        child : ClipOval(
-        child : SizedBox(
-          width : 60.0,
-          height : 60.0,
-          child: Image.asset("assets/images/development.png",
-           fit:BoxFit.cover,
-          ),
-        ),
-      ),
-      ),
     );
   }
-
 }
